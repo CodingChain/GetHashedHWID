0c0c665abd13c1fa14a030e3922d81cf1a3140e8
CodingChain <43908424+CodingChain@users.noreply.github.com>
2018-10-06 19:43:34 +0200

    Add files via upload

---
+++ GetHashedHwid/GetHWID.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Management
+Imports System
+Imports System.Text
+Imports System.Security.Cryptography
+Public Class GetHWID
+
+    Dim hw As New ClsComputerInfo
+    Public hwid As String = Strings.UCase(hw.getMD5Hash(hw.GetProcessorId() & hw.GetVolumeSerial("C") & hw.GetMotherBoardID() & hw.GetMACAddress()))
+
+End Class
